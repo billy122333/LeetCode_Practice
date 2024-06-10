f889815fbf74c4eba9fe0169f799c88eb285d343
Kai-bo Chang <75492436+billy122333@users.noreply.github.com>
2024-06-10 21:29:04 +0800

    Create 1051. Height Checker.cpp

---
+++ easy/1051. Height Checker.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> sorted_heights ;
+        sorted_heights.assign(heights.begin(), heights.end());
+        sort(sorted_heights.begin(), sorted_heights.end());
+        int ans = 0;
+        for(int i = 0; i< heights.size(); i++){
+            if(heights[i] != sorted_heights[i])
+                ans++;
+        }
+        return ans;
+    }
+};
